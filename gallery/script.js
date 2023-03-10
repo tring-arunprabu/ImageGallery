a240b254e7460a2adc83177a066ba1de97cdc23e
S-Arun0010 <123442099+S-Arun0010@users.noreply.github.com>
2023-03-11 00:01:52 +0530

    Add files via upload

---
+++ gallery/script.js
@@ -0,0 +1,30 @@
+var picture = document.getElementById("full-img");
+var image  = document.getElementById("image");
+ 
+const openimage =(pic) =>
+{
+   picture.style.display = "flex";
+   image.src = pic;
+}
+const closeimage =() =>
+{
+   picture.style.display = "none";
+}
+ 
+let r= 0;
+const Rotate = () =>
+{
+    r = r + 90;
+    image.style.transform = `rotate(${r}deg)`;
+}
+let z=0;
+const zoomin = () =>
+{
+    z= z + 0.5;
+    image.style.transform = `scale(${z})`;
+}
+const zoomout = () =>
+{
+    z= z - 0.5;
+    image.style.transform = `scale(${z})`;
+}
